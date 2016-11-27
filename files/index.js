5b033e9b1ca7a0be1e8235859b7badb50f26b1ef
Hayden Blair <haydenblair@Haydens-MacBook-Pro.local>
2016-11-27 18:38:49 -0500

    ok

--- files/index.js
+++ files/index.js
@@ -1,17 +1,20 @@
 var isClosed = false;
 $.ajax({
-    url: 'http://cnycentral.com/resources/ftptransfer/wstm/closings/closings.htm',
+    url: 'http://nbc25news.com/resources/ftptransfer/weyi/closings/closings.htm',
     success: function(data) {
         $(data).find(".orgname").each(function() {
             var schoolName = this.innerText;
             console.log(schoolName);
-            if (schoolName.indexOf("Marcellus") != -1) {
+            if (schoolName.indexOf("Bay City Public Schools") != -1) {
                 isClosed = true;
-                $('.isit').text("Marcellus is Closed!");
+              $('.isit').text("YES");
+                  $('.sub').text("School is Canceled.")
             }
         });
         if (isClosed != true) {
-            $('.isit').text("Marcellus isn't closed... but don't give up hope!");
+            $('.isit').text("NOPE");
+           
+ $('.sub').text("You have to go to school.")
         }
     }
 });
